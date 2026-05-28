a7e43cad2d87660a20e60ce696cb6d104d6b9e98
Guga Villar <grsv.21@gmail.com>
2026-05-28 20:26:25 -0300

    🐛 Payment method

--- src/components/Organisms/PaymentExternalForm/PaymentExternalForm.tsx
+++ src/components/Organisms/PaymentExternalForm/PaymentExternalForm.tsx
@@ -8,7 +8,7 @@ import { SelectField } from '@/components/Molecules'
 import type { ExternalParticipantFormProps, ExternalVolunteerFormProps } from '@/components/Templates'
 import type { FullSchemaType as FullSchemaTypeParticipant } from '@/components/Templates/ExternalParticipantForm/ExternalParticipantForm.schema'
 import type { FullSchemaType as FullSchemaTypeVolunteer } from '@/components/Templates/ExternalVolunteerForm/ExternalVolunteerForm.schema'
-import { PAYMENT_METHOD_EXTERNAL_OPTIONS, ShirtsAPI, ShirtsOptions } from '@/constants'
+import { MEMBERS, PAYMENT_METHOD_EXTERNAL_OPTIONS, ShirtsAPI, ShirtsOptions } from '@/constants'
 
 import { PaymentChoice } from './PaymentChoice'
 import { PaymentObservations } from './PaymentObservations'
@@ -40,6 +40,7 @@ export const PaymentExternalForm = ({
 	const isShirtSizeSpecial = watch('shirtSize') === ShirtsAPI.SPECIAL
 	const valueWithShirt =
 		isShirtSizeSpecial && registrationValueWithShirt ? registrationValueWithShirt + 5 : registrationValueWithShirt
+	const isShowPaymentMethod = Boolean(hasShirt) || inscriptionType !== MEMBERS.VOLUNTEER
 
 	return (
 		<div className="flex flex-col space-y-8">
@@ -48,17 +49,19 @@ export const PaymentExternalForm = ({
 				lastSundayFromEvent={lastSundayFromEvent}
 				registrationValue={registrationValue}
 			/>
-			<div className="grid grid-cols-1 gap-5 md:grid-cols-2">
-				<SelectField fieldName="withShirt" options={INSCRIPTION_OPTION} placeholder="Selecione uma opção">
-					Selecione sua opção de Inscrição
-				</SelectField>
-				{hasShirt === 'Yes' && (
-					<SelectField fieldName="shirtSize" options={ShirtsOptions} placeholder="Selecione uma opção">
-						Qual o tamanho?
+			{inscriptionType === MEMBERS.VOLUNTEER && (
+				<div className="grid grid-cols-1 gap-5 md:grid-cols-2">
+					<SelectField fieldName="withShirt" options={INSCRIPTION_OPTION} placeholder="Selecione uma opção">
+						Selecione sua opção de Inscrição
 					</SelectField>
-				)}
-			</div>
-			{hasShirt && (
+					{hasShirt === 'Yes' && (
+						<SelectField fieldName="shirtSize" options={ShirtsOptions} placeholder="Selecione uma opção">
+							Qual o tamanho?
+						</SelectField>
+					)}
+				</div>
+			)}
+			{isShowPaymentMethod && (
 				<SelectField
 					fieldName="paymentMethod"
 					options={PAYMENT_METHOD_EXTERNAL_OPTIONS}
